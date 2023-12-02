425ea51c17f688f95a30f3974af335d6f7878b97
nitinn_rajj <rajnitin088@gmail.com>
2023-12-02 20:11:50 +0530

    I added one line

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ int main ( int argumentcount , char *argumentval[] )
      SDL_Window *window = SDL_CreateWindow("First SDL Window" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , height , width , SDL_WINDOW_SHOWN);
      SDL_Renderer *renderer = SDL_CreateRenderer(window, -1 ,0);
      cout<<"Nitin";
+
+     cout<<"I joined.";
      
 
 return 0;     
